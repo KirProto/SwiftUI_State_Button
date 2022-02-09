bd7b570b4a620b90d2fbfcab9c4f5e87860732bc
Kir Proto Q <kir.proto@ya.ru>
2022-02-09 09:06:36 +0500

    Initial Commit

---
+++ Yan_SwiftUI_State_Button/Yan_SwiftUI_State_ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_State_ButtonApp.swift
+//  Yan_SwiftUI_State_Button
+//
+//  Created by KIR Q on 2022.02.09.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_State_ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
